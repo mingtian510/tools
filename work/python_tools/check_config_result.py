9c87ca3c517a60ad8f4cd15cfd586435cd057ef0
mingtian510 <397498896@qq.com>
2019-05-21 14:35:30 +0800

    Signed-off-by: mingtian510 <397498896@qq.com>

--- work/python_tools/check_config_result.py
+++
@@ -1,5 +0,0 @@
-
-def check_config_result(qian_data,hou_data):
-    for key in qian_data.keys():
-        assert qian_data[key] == hou_data[key]
-
--- work/python_tools/config_check/check_config_result.py
+++ work/python_tools/config_check/check_config_result.py
@@ -1,4 +1,3 @@
-
 def check_config_result(qian_data,hou_data):
     for key in qian_data.keys():
         assert qian_data[key] == hou_data[key]
